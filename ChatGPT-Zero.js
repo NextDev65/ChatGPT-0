7acdecbe31cf15d7f2ac5331c3fc397ee6cfb89e
NextDev65 <12612637+NextDev65@users.noreply.github.com>
2025-04-24 23:08:34 -0500

    style: add base box-shadow for smoother transitions; fine-tune hover effect
    - Introduced initial transparent `box-shadow` state for dropdown to ensure smooth animation on hover/focus
    - Adjusted hover inset shadow to match visual consistency with default/focus states
    - Bumped version to 0.36

--- ChatGPT-Zero.js
+++ ChatGPT-Zero.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ChatGPT Zero
 // @namespace    https://github.com/NextDev65/
-// @version      0.35
+// @version      0.36
 // @description  hot switch models on ChatGPT
 // @author       NextDev65
 // @downloadURL  https://raw.githubusercontent.com/NextDev65/ChatGPT-0/main/ChatGPT-Zero.js
@@ -39,11 +39,12 @@
                 background-color: #212121;
                 color: #fff;
                 outline: none;
+                box-shadow: 0 0 0 0 rgba(33, 33, 33, 0.0) inset, 0 0 5px 0 rgba(33, 33, 33, 0);
                 transition: background-color 0.2s cubic-bezier(0.4, 0, 0.2, 1), box-shadow 0.4s cubic-bezier(0.4, 0, 0.2, 1);
             }
             #chatgpt-model-switcher:hover {
                 background-color: #2f2f2f;
-                box-shadow: 0 0 5px 0 rgba(255, 255, 255, 0.2);
+                box-shadow: 0 0 2.5px 0 rgba(255, 255, 255, 0.0) inset, 0 0 5px 0 rgba(255, 255, 255, 0.2);
             }
             #chatgpt-model-switcher:focus {
                 outline: none;
